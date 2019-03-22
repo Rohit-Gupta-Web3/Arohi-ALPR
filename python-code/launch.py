ebb670278fc07861800db10db349b03e285d9036
Rohit-Gupta <rohit.gupta@iic.ac.in>
2019-03-22 17:52:51 +0530

    22 March 2019

---
+++ python-code/launch.py
@@ -0,0 +1,72 @@
+#!/bin/usr/env python3
+from tkinter import *
+import tkinter.filedialog as filedialog, os, subprocess as sub, sqlite3, locale, shutil,time
+from datetime import datetime
+locale.setlocale(locale.LC_ALL, 'C')
+
+def folder_move(buss,dev):
+		c=sqlite3.connect("Aarohi.db")
+		cu = c.cursor()
+		cu.execute("SELECT ccode FROM Bussiness WHERE bid=?", (buss,)) 
+		c_code=str(cu.fetchall()[0][0]).rstrip('\n')
+		cu.execute("SELECT infol FROM Bussiness WHERE bid=?", (buss,)) 
+		input_folder=str(cu.fetchall()[0][0]).rstrip('\n')+"/"+dev+"/"
+		cu.execute("SELECT pfol FROM Bussiness WHERE bid=?", (buss,)) 
+		processed_folder=str(cu.fetchall()[0][0]).rstrip('\n')+"/"
+		cu.execute("SELECT unfol FROM Bussiness WHERE bid=?", (buss,)) 
+		unprocessed_folder=str(cu.fetchall()[0][0]).rstrip('\n')+"/"
+		cu.execute("SELECT Dip FROM Device WHERE Did=?", (dev,)) 
+		cip=str(cu.fetchall()[0][0]).rstrip('\n')
+		pwd = os.path.dirname(os.path.realpath(__file__)).rstrip('\n')
+		onlyfiles = [ f for f in os.listdir(input_folder) if os.path.isfile(os.path.join(input_folder, f)) ]
+		if len(onlyfiles)==0:
+			time.sleep(10)
+			response = os.system("ping -c 1 " + cip)
+			if response >5:
+				print("No files to process")
+				time.sleep(1)
+				tkinter.messagebox.showerror('Camera is not replying!')
+		i = 0
+		while i < len(onlyfiles):
+			os.chdir(input_folder)
+			intime=time.ctime(os.path.getctime(onlyfiles[i]))
+			p1 = sub.Popen(['alpr', '-c',c_code,'--clock', onlyfiles[i]], stdout=sub.PIPE)
+			output = p1.communicate()[0]
+			old_nm=input_folder+str(onlyfiles[i])
+			output=str(output.decode('utf-8'))
+			output=output.split()
+			a=0
+			while a< len(output):
+				if output[a] == "license":
+					print("file not processed")
+					c.execute("insert into unprocessed values (?,?,?,?,?) ", (buss, dev, onlyfiles[i], intime, str(output)))
+					c.commit()
+					shutil.move(old_nm,unprocessed_folder)
+				elif output[a] == "results":
+					print(buss, dev, output[15], output[17])
+					c.execute("insert into alpr values (?,?,?,?,?,?) ", (buss, dev, intime, str(output[5]), str(output[15]), str(output[17])))
+					c.commit()
+					new_nm=processed_folder+(str(output[15])+".jpg")
+					shutil.move(old_nm,new_nm)
+				a=a+1
+			os.chdir(pwd)
+			i += 1
+
+if __name__=="__main__":
+	buss="rohit"
+	c=sqlite3.connect("Aarohi.db")
+	cu = c.cursor()
+	cu.execute("SELECT bid FROM Bussiness WHERE bid=?", (buss,)) 
+	bid=str(cu.fetchall()[0][0]).rstrip('\n')
+	cu.execute("SELECT Did FROM device WHERE bid=?", (buss,)) 
+	d=cu.fetchall()
+	c.execute('''CREATE TABLE if not exists alpr
+			(Bid text(15), Did text(15), In_time blob, processing_time blob, Plate_no text(20), Confidence float(4))''')
+	c.execute('''CREATE TABLE if not exists unprocessed
+			(Bid text(15), Did text(15), file_name blob, In_time blob, reason text(100))''')
+	c.commit()
+	while(1):
+		for k in d:
+			for a in k:
+				did=str(a)
+				folder_move(bid,did)
---
+++ python-code/login.py
@@ -0,0 +1,76 @@
+#!/bin/usr/env python
+from tkinter import *
+import tkinter.filedialog as filedialog, tkinter.messagebox, os, subprocess as sub, sqlite3
+
+master = Tk()
+master.title('Launch Window')
+master.geometry('350x300')
+user = 'aarohi'
+pas = 'aarohi'
+
+def login():
+	global master
+	if not os.path.isfile('login.txt'):
+		sub.call(['touch', 'login.txt'])
+	f = open('login.txt', 'w')
+	f.write(user)
+	f.write(pas)
+	f = open('login.txt', 'r')
+	logi = f.read().rstrip('\n')
+	vlogi = uid.get() + pswd.get()
+	if str(logi) == str(vlogi):
+		Button(master, text='Bussiness Setup', command=buss_fn).grid(row=5, column=0, sticky=N, pady=4)
+		Button(master, text='Device Setup', command=dev_fn).grid(row=5, column=1, sticky=N, pady=4)
+		Button(master, text='Continuing with APLR', command=alpr).grid(row=6, column=0, sticky=N, pady=4)
+		master.update()
+	else:
+		tkinter.messagebox.showerror('Please check the username and password!')
+	f.close()
+
+def exit():
+	result = tkinter.messagebox.askquestion('Quit', 'Are You Sure?', icon='warning')
+	if result == 'yes':
+		master.quit()
+
+def buss_fn():
+	sub.call(['python3', 'bussiness.cpython-36.pyc'])
+
+def dev_fn():
+	sub.call(['python3', 'device.cpython-36.pyc'])
+	result = tkinter.messagebox.askquestion('Device Setup', 'Do you want to enter more?', icon='info')
+	if result == 'yes':
+		sub.call(['python3', 'device.cpython-36.pyc'])
+
+def alpr():
+	con=sqlite3.connect("Aarohi.db")
+	cursor = con.cursor()
+	cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Bussiness';")
+	if len(cursor.fetchall())==0:
+		tkinter.messagebox.showinfo('Please Create ateast one bussiness id ')
+		sub.call(['python3', 'bussiness.cpython-36.pyc'])	
+		cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='Device';")
+		if len(cursor.fetchall())==0:
+			tkinter.messagebox.showinfo('Please Create ateast one device id ')
+			sub.call(['python3', 'device.cpython-36.pyc'])
+	else:
+		sub.call(['python3', 'launch.cpython-36.pyc'])
+
+def main():
+	global uid,pswd,lid
+	Label(master, text=' Username *').grid(row=0, sticky=N, pady=4)
+	Label(master, text=' Password *').grid(row=1, sticky=N, pady=4)
+	Label(master, text=' License ID *').grid(row=2, sticky=N, pady=4)
+
+	uid = Entry(master)
+	pswd = Entry(master,show="*")
+	lid = Entry(master)
+	uid.grid(row=0, column=1, sticky=N, pady=4)
+	pswd.grid(row=1, column=1, sticky=N, pady=4)
+	lid.grid(row=2, column=1, sticky=N, pady=4)
+
+	Button(master, text='login', command=login).grid(row=4, column=0, sticky=N, pady=4)
+	Button(master, text='Exit', command=exit).grid(row=4, column=1, sticky=N, pady=4)
+
+if __name__=="__main__":
+	main()
+	mainloop()
