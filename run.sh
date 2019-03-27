3c386c09c396de9774f02bc4972bd695a9be3647
Rohit-Gupta <rohit.gupta@iic.ac.in>
2019-03-27 13:35:13 +0530

    27 March 2019

---
+++ run.sh
@@ -0,0 +1,13 @@
+{
+echo '#!/usr/bin/env xdg-open'
+echo '[Desktop Entry]'
+echo 'Version=1.0'
+echo 'Type=Application'
+echo 'Terminal=false'
+echo 'Exec=python3 login.cpython-36.pyc'
+echo 'Name=Aarohi Impex'
+echo 'Icon=.'
+} > Aarohi.desktop
+
+
+#`grep '^Exec' Aarohi.desktop | tail -1 | sed 's/^Exec=//' | sed 's/%.//' | sed 's/^"//g' | sed 's/" *$//g'` &
